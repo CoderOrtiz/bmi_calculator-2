623a0f1ea989305bb8c77143571d047e6dce497e
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-04-05 12:05:58 -0400

    Fixed Rounding Logic Bug.

--- script.js
+++ script.js
@@ -9,26 +9,26 @@ const user = {
     // When "calcUserBMI" is Called, User's BMI will be Calculated and be Advised if They Are at a Healthy Weight
     calcUserBMI: function () {
         this.userBMI = (this.yourMass / (this.yourHeight ** 2)) * 703;
-
+        this.userBMIRounded = Math.round(this.userBMI);
         //  This Information Gets Displayed under "user" Class
-        document.querySelector('.user').textContent = `${this.userBMI ? 'Your BMI is approximately ' + Math.round(this.userBMI) + '.' : 'You entered an invalid input.'}`;
+        document.querySelector('.user').textContent = `${this.userBMIRounded ? 'Your BMI is approximately ' + this.userBMIRounded + '.' : 'You entered an invalid input.'}`;
 
         //  This Information Gets Displayed under "users_status" Class
-        if (this.userBMI <= 18) {
+        if (this.userBMIRounded <= 18) {
             document.querySelector('.users_status').textContent = 'You are underweight.';
         }
-        else if ((this.userBMI >= 19) && (this.userBMI <= 25)) {
+        else if ((this.userBMIRounded >= 19) && (this.userBMIRounded <= 25)) {
             document.querySelector('.users_status').textContent = 'You are at a healthy weight.';
         }
-        else if ((this.userBMI >= 26) && (this.userBMI <= 30)) {
+        else if ((this.userBMIRounded >= 26) && (this.userBMIRounded <= 30)) {
             document.querySelector('.users_status').textContent = 'You are overweight.';
         }
-        else if (this.userBMI > 31) {
+        else if (this.userBMIRounded > 31) {
             document.querySelector('.users_status').textContent = `${this.you}'s obese.`;
         }
         else {    
         }
-        return this.userBMI;
+        return this.userBMIRounded;
     },
 };
 
@@ -43,24 +43,26 @@ const yourFriend = {
      // When "calcFriendsBMI" is Called, Friend's BMI will be Calculated and be Advised if They Are at a Healthy Weight
     calcFriendsBMI: function () {
         this.companionsBMI = (this.companionsWeight / (this.companionsHeight ** 2)) * 703;
-        document.querySelector('.your_friend').textContent = `${this.companionsBMI ? this.companionsName + "'s BMI is approximately " + Math.round(this.companionsBMI) + "." : "You entered an invalid input."}`;
+        this.companionsBMIRounded = Math.round(this.companionsBMI);
+        document.querySelector('.your_friend').textContent = `${this.companionsBMI ? this.companionsName + "'s BMI is approximately " + this.companionsBMIRounded + "." : "You entered an invalid input."}`;
         
         //  This Information Gets Displayed under "your_friends_status" Class
-        if (this.companionsBMI <= 18) {
+        if (this.companionsBMIRounded <= 18) {
             document.querySelector('.your_friends_status').textContent = `${this.companionsName}'s underweight.`;
         }
-        else if ((this.companionsBMI >= 19) && (this.companionsBMI <= 25)) {
+        else if ((this.companionsBMIRounded >= 19) && (this.companionsBMIRounded <= 25)) {
             document.querySelector('.your_friends_status').textContent = `${this.companionsName}'s at a healthy weight.`;
         }
-        else if ((this.companionsBMI >= 26) && (this.companionsBMI <= 30)) {
+        else if ((this.companionsBMIRounded >= 26) && (this.companionsBMIRounded <= 30)) {
+            console.log(this.companionsBMIRounded)
             document.querySelector('.your_friends_status').textContent = `${this.companionsName}'s overweight.`;
         }
-        else if (this.companionsBMI > 31) {
+        else if (this.companionsBMIRounded > 31) {
         document.querySelector('.your_friends_status').textContent = `${this.companionsName}'s obese.`;
         }
         else {
         }
-        return this.companionsBMI;
+        return this.companionsBMIRounded;
     },
 };
 
@@ -68,19 +70,19 @@ const yourFriend = {
 // When the "results" Function is Called, If Else Statements Determine Who has the Higher BMI and Will Change the Color of the h6
 function results() {
 
-    if ((user.userBMI === undefined) || (yourFriend.companionsBMI === undefined)) {
+    if ((user.userBMIRounded === undefined) || (yourFriend.companionsBMIRounded === undefined)) {
         document.querySelector('.results').textContent = `You must first calculate your results, and then ${yourFriend.companionsName}'s results, to see the final results.`;
     }
-    else if (user.userBMI === yourFriend.companionsBMI) {
-        document.querySelector('.results').textContent = `You both have a BMI of approximately ${Math.round(user.userBMI)}.`;
+    else if (user.userBMIRounded === yourFriend.companionsBMIRounded) {
+        document.querySelector('.results').textContent = `You both have a BMI of approximately ${user.userBMIRounded}.`;
         document.querySelector('.results').style.color = 'yellow';
     }
-    else if (user.userBMI > yourFriend.companionsBMI) {
-        document.querySelector('.results').textContent = `You have the higher BMI of approximately ${Math.round(user.userBMI)}.`;
+    else if (user.userBMIRounded > yourFriend.companionsBMIRounded) {
+        document.querySelector('.results').textContent = `You have the higher BMI of approximately ${user.userBMIRounded}.`;
         document.querySelector('.results').style.color = '#890F0D';
     }
-    else if (user.userBMI < yourFriend.companionsBMI) {
-        document.querySelector('.results').textContent = `${yourFriend.companionsName} has a higher BMI of approximately ${Math.round(yourFriend.companionsBMI)}.`;
+    else if (user.userBMIRounded < yourFriend.companionsBMIRounded) {
+        document.querySelector('.results').textContent = `${yourFriend.companionsName} has a higher BMI of approximately ${yourFriend.companionsBMIRounded}.`;
         document.querySelector('.results').style.color = '#BAFFB4';
     }
     else{
